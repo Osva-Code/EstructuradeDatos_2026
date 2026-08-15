bf89e1dac460bad6f67016b90e543bce171bdf35
Nacatl Osvaldo Diaz de la Vega Caballero <osvadvc@gmail.com>
2026-08-15 04:41:17 -0600

    feat: definicion de struct Proveedor y Producto anidado

--- FastCartCore/Program.cs
+++ FastCartCore/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FastCartCore
+{
+    public struct Proveedor
+    {
+        public int IdProveedor;
+        public string NombreCorporativo;
+    }
+
+    public struct Producto
+    {
+        public int SKU;
+        public string Nombre;
+        public double Precio;
+        public int Stock;
+        public Proveedor DatosProveedor;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("FastCart Backend Core - Fase 1 Inicializada");
+        }
+    }
+}
